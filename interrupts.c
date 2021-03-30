87b6fbc45141704154ae205ac8d34d991f2f0172
bjs2864 <36084911+bjs2864@users.noreply.github.com>
2021-03-30 14:09:16 -0400

    Updated with some basic SPI code
    Needs more research and code, but this is some of the initialization required

--- interrupts.c
+++ interrupts.c
@@ -1,7 +1,9 @@
 #include "interrupts.h"
 #include "MK64F12.h"
 #include "uart.h"
+#include "spi.h"
 
 void NVIC_init(void){
 	NVIC_EnableIRQ(UART0_RX_TX_IRQn);
+	NVIC_EnableIRQ(SPI0_IRQn);
 }
--- spi.h
+++ spi.h
@@ -1,4 +1,4 @@
 #ifndef SPI_H
 #define SPI_H
-
+void spi0_init(void);
 #endif
